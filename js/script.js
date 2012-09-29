128ee31873600ceafba2fc756c6f3e4f8f5aa9f5
Pieter Colpaert <pieterc@member.fsf.org>
2012-09-30 00:32:54 +0200

    added the extra info to the right

--- js/script.js
+++ js/script.js
@@ -5,6 +5,28 @@
 $('input').click(function(){
     if($('#frame')){
         $('#frame').attr('src',$('input[name=datasets]:checked').val() + ".map");
+        /* Now also provide the information about several parkings in the aside */
+        // For instance http://park.irail.be/data/Parking/Ghent/itsps/Operator/OffstreetParking.json
+        $.ajax({
+            url: "http://park.irail.be/data/Parking/" + $('input[name=datasets]:checked').attr("id") +"/itsps/Operator/OffstreetParking.json" ,
+            type: "GET",
+            error: function(){
+                $("#asidetitle").html("Sorry, I've done something wrong. As this is a proof of concept, please ask someone for help.");
+            }
+        }).done(function(data) { 
+            $("#asidetitle").html($('input[name=datasets]:checked').attr("id"));
+            /* Now also provide the information about several parkings in the aside */
+            // For instance http://park.irail.be/data/Parking/Ghent/itsps/Operator/OffstreetParking.json
+            $("#explanation").html("");
+            $.each(data["OffstreetParking"], function(index, value){
+                $("#explanation").append("<h4>" + value["GeneralInfo"]["IDInfo"]["Name"] + "</h4>"); 
+                $("#explanation").append("<p><strong>Capacity</strong>: " + value["Capacity"] + "<br/>");
+                $("#explanation").append("<strong>Parking type</strong>: " + value["ParkingType"] + "<br/>");
+                $("#explanation").append("<strong>Permitted vehicles</strong>: " + value["PermittedVehicle"] + "<br/>");
+                $("#explanation").append("<strong>Floors</strong>: " + value["Floors"] + "</p>");
+            });
+        });        
+
     }
 
 });
